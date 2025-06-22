f0576374307d8c1c495bf21c61c88c649caaf072
Mohamad Yasser Ahmad Ghazy <mohamadyasser515@gmail.com>
2025-06-22 13:37:59 +0300

    Initial commit: Add basic structure, styling for portfolio website, Adding Assets and create some js functions.
    This repository contains the source code for my personal portfolio website, which highlights my skills, and experience. The website is built using HTML, CSS, and JavaScript to demonstrate my proficiency in front-end development. The portfolio will be updated as I work on more projects and gain experience.

---
+++ script.js
@@ -0,0 +1,50 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+function showText(logo) {
+  const infoBox = document.getElementById('infoBox');
+  const infoText = document.getElementById('infoText');
+
+  if (logo === 'google') {
+    infoText.textContent = "I am Certified Data Analytics Professional from Google";
+  } else if (logo === 'postgrad') {
+    infoText.textContent = "Currently pursuing my M.Sc in Information Systems from Ain-Shams University Faculty of Computer Science";
+  } else if (logo === 'eva-pharma') {
+    infoText.textContent = "I have 4+ years of experience in Data Analytics at Eva Pharma, one of the fastest-growing pharmaceutical company in the region.";
+  } else if (logo === 'must') {
+    infoText.textContent = "I got my B.Sc. in Biotechnology from Misr University for Science and Technology (MUST) with GPA 3.89 (Excellent with Honors)";
+  }
+}
+
+
+const projectImg = document.getElementById('project-img');
+const overlay = document.getElementById('overlay');
+
+// When the mouse enters the image area, show the popup effect and dim the background
+projectImg.addEventListener('mouseenter', function() {
+    const gifSource = projectImg.getAttribute('data-gif');  // Get GIF URL from data attribute
+    projectImg.src = gifSource;  // Set the src to the GIF
+
+    // Add 'popup' class to enlarge and center the image
+    projectImg.classList.add('popup');
+
+    // Show the overlay (dimmed background)
+    overlay.style.opacity = '1';
+    overlay.style.visibility = 'visible';
+});
+
+// When the mouse leaves the image area, remove the popup effect and restore the image
+projectImg.addEventListener('mouseleave', function() {
+    const staticImageSource = './assets/project-1.png';  // Static image fallback
+    projectImg.src = staticImageSource;  // Reset the image to static
+
+    // Remove 'popup' class to return the image to normal
+    projectImg.classList.remove('popup');
+
+    // Hide the overlay
+    overlay.style.opacity = '0';
+    overlay.style.visibility = 'hidden';
+});
